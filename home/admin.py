e444863a774010f014b36fb99407306f9a9e6ee6
cankartal3 <ugurcankartal9@gmail.com>
2020-05-05 18:25:21 +0300

    Contact form bize ulaşın formu eklendi.

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from home.models import Settings
+from home.models import Settings, ContactFormMessage
 
+
+class ContactFormMessageAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'subject','message','note', 'status']
+    list_filter = ['status']
+
+admin.site.register(ContactFormMessage,ContactFormMessageAdmin)
 admin.site.register(Settings)
--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import Settings
+from home.models import Settings, ContactFormuu, ContactFormMessage
 
 
 def index(request):
@@ -21,6 +22,21 @@ def referanslarimiz(request):
     return render(request, 'referanslarimiz.html', context)
 
 def iletisim(request):
+
+    if request.method == 'POST':  # form post edildiyse
+        form = ContactFormuu(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()  # model ile bağlantı kur
+            data.name = form.cleaned_data['name']  # formdan bilgiyi al
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()  # veri tabanına kaydet
+            messages.success(request, "Mesajınız başarılı bir şekilde gönderilmiştir. Teşekkür ederiz.")
+            return HttpResponseRedirect('/iletisim')
+
     settings = Settings.objects.get(pk=1)
-    context = {'settings': settings, 'page':'iletisim'}
+    form = ContactFormuu()
+    context = {'settings': settings, 'form':form}
     return render(request, 'iletisim.html', context)
