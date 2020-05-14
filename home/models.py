fbff9fced27748ed095eadefbd875effe23dab58
cankartal3 <ugurcankartal9@gmail.com>
2020-05-14 19:47:09 +0300

    Uye Profile ile Ek Bilgiler ve resim eklendi.

--- home/models.py
+++ home/models.py
@@ -1,8 +1,10 @@
 from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 from django.forms import ModelForm, TextInput, Textarea
+from django.utils.safestring import mark_safe
 
 
 class Settings(models.Model):
@@ -69,13 +71,25 @@ class ContactFormuu(ModelForm):
         }
 
 
+class UserProfile(models.Model):
+        user = models.OneToOneField(User, on_delete=models.CASCADE)
+        phone = models.CharField(blank=True, max_length=20)
+        address = models.CharField(blank=True, max_length=20)
+        city = models.CharField(blank=True, max_length=20)
+        country = models.CharField(blank=True, max_length=20)
+        image = models.ImageField(blank=True, upload_to='images/users/')
 
+        def __str__(self):
+            return self.user.username
 
+        def user_name(self):
+            return '['+self.user.username + '] ' + self.user.first_name+ ' ' + self.user.last_name
 
+        def image_tag(self):
+            return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+        image_tag.short_description = 'Image'
 
-
-
-
-
-
-
+class UserProfileForm(ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['phone', 'address', 'city', 'country','image']
